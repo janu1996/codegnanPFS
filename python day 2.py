c199976d21f1ff0c5a801fccf1d1df2a85409f6c
Jahnavi Devi <159162837+janu1996@users.noreply.github.com>
2026-06-23 20:11:14 +0530

    Add files via upload
    git commit -m "docs: add Python Day 2 learning notes
    
    - Added arithmetic operators
    - Added assignment operators
    - Added comparison operators
    - Added logical operators
    - Added identity and membership operators
    - Added bitwise operators
    - Included variable naming convention examples"

---
+++ python day 2.py
@@ -0,0 +1,93 @@
+'''
+  operators
+  ------------
+  1. Arithmetic operator
+   used for mathematically operators
+      +, - , * ,/ ,**,%,//
+      ex:-
+      a=8
+      b=10
+      print(a+b)
+
+      ---> ** is used for power. it  is called as exponitial
+      ---> % is used to find percentage
+      
+  2. Assignment operator
+    used to assign the value to datatype (datatype  to store in variable)
+    =,+=,-=,*=,%=
+    ex:-
+    a=20
+    a+=4 # a= a+4
+    print(a)
+   ---> += is  used to increment
+   ---> -= is used to decrement
+   ---> *= a =a%4
+
+  3. Comparision operator
+  used for comparing   
+  ==,!=,>=,<=,<,>
+   ex:-
+   a=20
+   b=7
+   print(a==b)
+ note:imp question
+ Q)what is difference between == and is
+ A)== looks for same value where is look for same object
+  4. Logical operator
+   and---> returns true if both are true....
+   or ---->  if any  one condition satisfies then it prints true
+   not----> expected output will be reverse
+ ex:-
+a=20
+b=7
+c=89
+print(a>=b and b<=c)
+print(a>=b and b>=c)
+print(a>=b or b>=c)
+print(not(a>=b or b>=c))
+
+
+  5. Identity operator
+
+  is---> same object or not
+  is not----> not the same object ( we will get reverse output)
+  ex:
+a=20
+b=20
+print(a==b)
+print(a is b)
+print(a is not b)
+  
+  6. Membership operator
+  in---> check weather it is present(inside) or not
+  not in---> shows opposite to output
+ex:-
+a=[1,2,3,4,5]
+print(2 in a)
+print(12 not in a)
+  
+  7. Bitwise operator
+   based on binary operators
+  & (and),>>,<<,|(or),^,&&,||
+  ex:
+  
+print(5 & 4)
+print(5 | 3)
+print(5 >> 2)
+print(5 << 1)
+-------------------------------------------
+why we use naming conventios for variables
+to avoid the confusion and to know it easily in which variable wha data has stored we use naming conventions instead to a,b
+ex:
+SBI_vizag_=[]
+ICIC_vizag=[]
+
+
+  '''
+
+
+
+
+  
+
+
