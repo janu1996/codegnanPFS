9c57c4fd770e7eae655601ff4ab6c699dfb28601
Jahnavi Devi <159162837+janu1996@users.noreply.github.com>
2026-06-26 12:43:00 +0530

    Add files via upload
    Added Day 4 Python notes on Lists and Tuples with detailed explanations, examples, and built-in methods.
    Documented Python List and Tuple concepts, including mutability, indexing, sorting, and common operations.
    Implemented practice examples for Python Lists and Tuples, covering list methods, nested indexing, and sorting techniques.
    Updated Python learning notes with comprehensive examples of List methods (append, extend, pop, remove, insert, sort, sorted) and Tuple methods (index, count).
    Enhanced Day 4 Python practice by exploring List manipulation, Tuple operations, mutable vs immutable data types, and nested collections.
    Added detailed Python examples demonstrating List operations, Tuple methods, indexing, and data manipulation concepts.
    Completed Day 4 Python practice covering Lists, Tuples, collection methods, sorting, and indexing with clear examples.
    Expanded Python fundamentals with hands-on examples of Lists, Tuples, mutability, and built-in collection methods.

---
+++ python day 4.py
@@ -0,0 +1,135 @@
+'''
+Lis is a collection of different dattypes
+
+List datatypes
+---> List is a collection of different datatypes that are enclosed(represented)in []
+seperated by commas(,)
+---> list is muttable
+ex:-
+all_type=[1,'python',[1,2])
+
+immutable ->the change we done will not stay(change)
+
+so="python is a language"
+prtint(so.replace('python','java'))
+prtint(so.replace('python','c'))
+prtint(so)
+
+muttable -> the change we done will be stay(modify)
+
+any_ = [1,2,3,4]
+print(any_)
+any_.append(5)
+print(any_)
+any_.append(10)
+print(any_)
+----------------
+Differnce b/w muttable and immutable
+
+muttable                                         immuttable
+-----------                                  ---------------------
+-> the datatype can be mofify                -->can't be modified
+eg:- List                                       eg:- string                         
+any_ = [1,2,3,4]                                    so="python is a language"
+print(any_)                                         prtint(so.replace('python','java'))
+any_.append(5)                                      prtint(so.replace('python','c'))
+print(any_)                                         prtint(so)
+any_.append(10)
+print(any_)                                     
+-------------------------------------------------------------------------------                                                 
+                                                                                              
+methods in list
+
+1.append()
+---> This is used to add a new item into list but it will add in the last index position
+we can only add one item
+ela iste ala add chestadhi(gives in on eindex value)
+eg:-
+any_ = [1,2,3,4]
+print(any_)
+any_.append(5)
+print(any_)
+any_.append(10)
+print(any_)
+any_.append('python')
+print(any_)
+----------------
+2.extend()
+--> This is also add aitem in the last index position,but it will give each value in the
+    each index position
+---> It will take only  list ,tuple and string  
+each character ki seperate index posistion istadhi(gives seperate index to each character)
+ex:-
+any_ = [1,2,3,4]
+any_.extend([23,90])
+any_.extend('python')
+print(any_)
+print(len(any_))
+------------------------------
+indexing in list
+any_ = [1,2,'python is a language',[45,78,"Java is a language",[1,23],90],'Hello']
+print(any_[3][3][1])
+-----------------------------------------------------
+3.pop()
+-->uses index value to delete
+--> used to delete the item from the list,but it will delete based on index position
+syntax:- variable_name.pop(index posistion)
+eg:-
+any_ = [1,2,45,78,23,90]
+any_.pop(2)
+print(any_)
+-------------------------------------------
+4.remove()
+-->uses direct value to delete
+-->used to delete the item from the list,but it will delete direct value from list
+syntax:- variable_name.remove(value need to be delete)
+eg:-
+any_ = [1,2,45,78,23,90]
+any_.remove(1)
+print(any_)
+-----------------------------------------
+5.insert()
+--> we can insert where we want the particular item
+syntax:-
+eg:-
+-----------
+6.sort()
+sorts permently
+eg:-
+any_=[78,45,34,1]
+any_.sort()
+print(any_)
+any_.append(10)
+print(any_)
+
+7.sorted()
+sorts only on runtime
+eg:-
+any_=[78,45,34,1]
+print(sorted(any_))
+-------------------------------
+
+Tuple
+---> tuple is a collection of different datatypes representd in () and seperated by comma(,)
+it is immuttable
+ex:-
+how = (1,2,3,4,"python",[4,5],(90,78))
+print(how)
+methos
+1. index()
+how = (1,2,3,4,"python",[4,5],(90,78))
+print(how.index('python'))
+2.count()
+how = (1,2,3,4,"python",[4,5],(90,78))
+print(how.count('python'))
+
+'''
+
+
+
+'''
+Task:- 1)write 2 examples for each method --> count,index,insert
+2)[56,[1,2],['python','java',['python is a language',153,90],[78,6],'I know c']] just find know using indexing and 153 also seperatly
+-----------------------
+'''
+
